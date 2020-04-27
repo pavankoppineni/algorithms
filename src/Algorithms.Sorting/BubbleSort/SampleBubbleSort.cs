fa6e2b3c4dbe5a4b5dce9af2b7c3b5141e439e95
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-04-27 22:30:28 +0530

    Added Bubble Sort
    1. Added code for bubble sort

---
+++ src/Algorithms.Sorting/BubbleSort/SampleBubbleSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting.BubbleSort
+{
+    public class SampleBubbleSort
+    {
+        private int[] values;
+        public SampleBubbleSort()
+        {
+            values = new int[5] { 4, 8, 1, 9, 5 };
+        }
+
+        public void Sort()
+        {
+            Console.WriteLine("Array before sorting");
+            foreach(var value in values)
+            {
+                Console.Write($"{value} ");
+            }
+            Console.WriteLine();
+
+            for (var i = 0; i < values.Length; i++)
+            {
+                for (var j = 1; j < values.Length - i; j++)
+                {
+                    if (values[j - 1] > values[j])
+                    {
+                        var temp = values[j - 1];
+                        values[j - 1] = values[j];
+                        values[j] = temp;
+                    }
+                }
+            }
+
+            Console.WriteLine("Array after sorting");
+            foreach (var value in values)
+            {
+                Console.Write($"{value} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
