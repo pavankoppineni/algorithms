b05a0a78a467dd317c835ec473be3c8a487ae036
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-05-24 19:54:17 +0530

    Fixed unit tests (#14)
    * Added Bubble Sort
    
    1. Added code for bubble sort
    
    * Added pattern matching project
    
    1. Added pattern mathing algorithms
    2. Added string problem project
    
    * Added longest substring without repeating chanracters problem
    
    * Update README.md
    
    * Added KMP Algorithm implementation
    
    1. Added KMP algorithm implmentation
    2. Added unit tests for KMP algorithm implementation
    
    * Removed kmp algorithm class
    
    * Added counting and merge sort.
    
    * Added data to ReadMe file
    
    * added data to read me file.
    
    * Added Radix sort
    
    * Added changes to README file.
    
    * Update RadixSort.cs
    
    * Added HeapSort and BubbleSortV2
    
    * Added unit tests for bubble sort v1
    
    * Moved bubble sort v2 to bubble sort folder
    
    * Fixed unit test issues
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ test/Algorithms.Sorting.Tests/BubbleSort/BubbleSortV1Tests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Sorting.BubbleSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting.Tests.BubbleSort
+{
+    [TestClass]
+    public class BubbleSortV1Tests
+    {
+        [TestMethod]
+        public void ArrayWithIntegers_Sort_ReturnsSortedIntegers()
+        {
+            //Arrange
+            var values = new int[] { 2, 4, 1, 8, 9, 3 };
+            var expectedValues = new int[] { 1, 2, 3, 4, 8, 9 };
+            var bubbleSort = new BubbleSortV1();
+
+            //Act
+            var actualValues = bubbleSort.Sort(values);
+
+            //Assert
+            for (var i = 0; i < values.Length; i++)
+            {
+                Assert.AreEqual(expectedValues[i], actualValues[i]);
+            }
+        }
+    }
+}
