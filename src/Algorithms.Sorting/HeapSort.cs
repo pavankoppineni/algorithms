ead3cde135d0a447911e8cac050895f80964c692
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-05-24 19:29:11 +0530

    Added HeapSort and BubbleSortV2

---
+++ src/Algorithms.Sorting/HeapSort.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting
+{
+    public class HeapSort
+    {
+        public int[] Sort(int[] values)
+        {
+            var length = values.Length;
+            for (var i = (length / 2) - 1; i >= 0; i--)
+            {
+                Heapify(values, i, values.Length);
+            }
+            for (var i = length - 1; i >= 0; i--)
+            {
+                var temp = values[0];
+                values[0] = values[i];
+                values[i] = temp;
+                Heapify(values, 0, i);
+            }
+            return values;
+        }
+
+        private void Heapify(int[] values, int root, int length)
+        {
+            var max = root;
+            var leftChild = 2 * root + 1;
+            var rightChild = 2 * root + 2;
+
+            if (leftChild < length)
+            {
+                if (values[leftChild] > values[max])
+                {
+                    max = leftChild;
+                }
+            }
+
+            if (rightChild < length)
+            {
+                if (values[rightChild] > values[max])
+                {
+                    max = rightChild;
+                }
+            }
+
+            if (root != max)
+            {
+                var temp = values[max];
+                values[max] = values[root];
+                values[root] = temp;
+
+                Heapify(values, max, length);
+            }
+        }
+    }
+}
---
+++ test/Algorithms.Sorting.Tests/HeapSortTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting.Tests
+{
+    [TestClass]
+    public class HeapSortTests
+    {
+        [TestMethod]
+        public void IntegerArray_Sort_ReturnsSortedArray()
+        {
+            //Arrange
+            var values = new int[] { 1, 8, 5, 4, 9, 10 };
+            var heapSort = new HeapSort();
+            var expectedArray = new int[] { 1, 4, 5, 8, 9, 10 };
+
+            //Act
+            var sortedArray = heapSort.Sort(values);
+
+            //Assert
+            for (var i = 0; i < values.Length; i++)
+            {
+                Assert.AreEqual(expectedArray[i], sortedArray[i]);
+            }
+        }
+    }
+}
