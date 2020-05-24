cf4eec5283d25d8333ae0a6300957906dba1f264
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-05-24 19:34:43 +0530

    Added HeapSort and BubbleSortV2 (#12)
    * Added Bubble Sort
    
    1. Added code for bubble sort
    
    * Added pattern matching project
    
    1. Added pattern mathing algorithms
    2. Added string problem project
    
    * Added longest substring without repeating chanracters problem
    
    * Update README.md
    
    * Added KMP Algorithm implementation
    
    1. Added KMP algorithm implmentation
    2. Added unit tests for KMP algorithm implementation
    
    * Removed kmp algorithm class
    
    * Added counting and merge sort.
    
    * Added data to ReadMe file
    
    * added data to read me file.
    
    * Added Radix sort
    
    * Added changes to README file.
    
    * Update RadixSort.cs
    
    * Added HeapSort and BubbleSortV2
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/Algorithms.Sorting/BubbleSortV2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting
+{
+    public class BubbleSortV2
+    {
+        public int[] Sort(int[] values)
+        {
+            var length = values.Length;
+            for (var i = 0; i < length; i++)
+            {
+                for (var j = 1; j < length - i; j++)
+                {
+                    if (values[j - 1] > values[j])
+                    {
+                        var temp = values[j - 1];
+                        values[j - 1] = values[j];
+                        values[j] = temp;
+                    }
+                }
+            }
+            return values;
+        }
+    }
+}
