e373ef07c9ab1e5885f496c7548f0c23c5a08d90
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-05-25 21:49:23 +0530

    Removed RadixSort file (#18)
    * Added Bubble Sort
    
    1. Added code for bubble sort
    
    * Added pattern matching project
    
    1. Added pattern mathing algorithms
    2. Added string problem project
    
    * Added longest substring without repeating chanracters problem
    
    * Update README.md
    
    * Added KMP Algorithm implementation
    
    1. Added KMP algorithm implmentation
    2. Added unit tests for KMP algorithm implementation
    
    * Removed kmp algorithm class
    
    * Added counting and merge sort.
    
    * Added data to ReadMe file
    
    * added data to read me file.
    
    * Added Radix sort
    
    * Added changes to README file.
    
    * Update RadixSort.cs
    
    * Added HeapSort and BubbleSortV2
    
    * Added unit tests for bubble sort v1
    
    * Moved bubble sort v2 to bubble sort folder
    
    * Fixed unit test issues
    
    * Added merge sort v2
    
    * Added counting sort v2
    
    * Modified README
    
    * Added radix sort v2
    
    * Fixed build issue
    
    * Removed RadixSort file
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ test/Algorithms.Sorting.Tests/RadixSort/RadixSortV2Tests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Sorting.RadixSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting.Tests.RadixSort
+{
+    [TestClass]
+    public class RadixSortV2Tests
+    {
+        [TestMethod]
+        public void IntegerArray_Sort_ReturnsSortedIntegers()
+        {
+            //Arrange
+            var values = new int[] { 141, 121, 131 };
+            var expectedValues = new int[] { 121, 131, 141 };
+            var sort = new RadixSortV2();
+
+            //Act
+            var actualValues = sort.Sort(values);
+
+            //Assert
+            for (var i = 0; i < values.Length; i++)
+            {
+                Assert.AreEqual(expectedValues[i], actualValues[i]);
+            }
+        }
+    }
+}
