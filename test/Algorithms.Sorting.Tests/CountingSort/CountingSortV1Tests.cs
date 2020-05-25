1e378a77a9a24b8f4d019bb6a5fc97e59505654f
Koppineni, Pavan K. (Tax&Accounting) <pavan.koppineni@thomsonreuters.com>
2020-05-25 19:07:48 +0530

    Added counting sort v2

--- test/Algorithms.Sorting.Tests/CountingSort/CountingSortV1Tests.cs
+++ test/Algorithms.Sorting.Tests/CountingSort/CountingSortV1Tests.cs
@@ -1,16 +1,17 @@
+using Algorithms.Sorting.CountingSort;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace Algorithms.Sorting.Tests
+namespace Algorithms.Sorting.Tests.CountingSort
 {
     [TestClass]
-    public class CountingSortTests
+    public class CountingSortV1Tests
     {
         [TestMethod]
         public void ArrayWithIntegers_Sort_ReturnsSortedArray()
         {
             //Arrange
             var values = new int[] { 4, 2, 2, 8, 3, 3, 1 };
-            var countingSort = new CountingSort();
+            var countingSort = new CountingSortV1();
             var expectedArray = new int[] { 1, 2, 2, 3, 3, 4, 8 };
             //Act
             var sortedArray = countingSort.Sort(values);
---
+++ test/Algorithms.Sorting.Tests/CountingSort/CountingSortV2Tests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Sorting.CountingSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting.Tests.CountingSort
+{
+    [TestClass]
+    public class CountingSortV2Tests
+    {
+        [TestMethod]
+        public void ArrayOfIntegers_Sort_ReturnsSortedArray()
+        {
+            //Arrange
+            var values = new int[] { 1, 10, 8, 6, 4, 9, 9, 10, 11 };
+            var expectedValues = new int[] { 1, 4, 6, 8, 9, 9, 10, 10, 11 };
+            var sort = new CountingSortV2();
+
+            //Act
+            var actualValues = sort.Sort(values);
+
+            //Assert
+            for (var i = 0; i < values.Length; i++)
+            {
+                Assert.AreEqual(expectedValues[i], actualValues[i]);
+            }
+        }
+    }
+}
