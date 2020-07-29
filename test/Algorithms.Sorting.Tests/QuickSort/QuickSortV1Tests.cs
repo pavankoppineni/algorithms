c3d37b7de56f5a0161e22c150c50c7237a733ad4
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-07-29 20:55:04 +0530

    Added quick sort and quick select algorithm (#19)
    * Added Bubble Sort
    
    1. Added code for bubble sort
    
    * Added pattern matching project
    
    1. Added pattern mathing algorithms
    2. Added string problem project
    
    * Added longest substring without repeating chanracters problem
    
    * Update README.md
    
    * Added KMP Algorithm implementation
    
    1. Added KMP algorithm implmentation
    2. Added unit tests for KMP algorithm implementation
    
    * Removed kmp algorithm class
    
    * Added counting and merge sort.
    
    * Added data to ReadMe file
    
    * added data to read me file.
    
    * Added Radix sort
    
    * Added changes to README file.
    
    * Update RadixSort.cs
    
    * Added HeapSort and BubbleSortV2
    
    * Added unit tests for bubble sort v1
    
    * Moved bubble sort v2 to bubble sort folder
    
    * Fixed unit test issues
    
    * Added merge sort v2
    
    * Added counting sort v2
    
    * Modified README
    
    * Added radix sort v2
    
    * Fixed build issue
    
    * Removed RadixSort file
    
    * Added quick sort and quick select algorithm
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ test/Algorithms.Sorting.Tests/QuickSort/QuickSortV1Tests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Sorting.QuickSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting.Tests.QuickSort
+{
+    [TestClass]
+    public class QuickSortV1Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_QuickSort_Then_ShouldSortArrayOfIntegers()
+        {
+            //Given
+            var values = new int[] { 8, 7, 6, 1, 0, 9, 2 };
+            var quickSort = new QuickSortV1();
+            var expectedValues = new int[] { 0, 1, 2, 6, 7, 8, 9 };
+
+            //When
+            var actualValues = quickSort.Sort(values);
+
+            //Then
+            for(var i = 0; i < values.Length; i++)
+            {
+                Assert.AreEqual(expectedValues[i], actualValues[i]);
+            }
+        }
+    }
+}
