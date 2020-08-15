ada7eb1aa65bdde05d8ad0791627e27973c48487
PavanKoppineniTR <53081930+PavanKoppineniTR@users.noreply.github.com>
2020-08-15 23:24:17 +0530

    Added KMP algorithm (#20)
    * Added Bubble Sort
    
    1. Added code for bubble sort
    
    * Added pattern matching project
    
    1. Added pattern mathing algorithms
    2. Added string problem project
    
    * Added longest substring without repeating chanracters problem
    
    * Update README.md
    
    * Added KMP Algorithm implementation
    
    1. Added KMP algorithm implmentation
    2. Added unit tests for KMP algorithm implementation
    
    * Removed kmp algorithm class
    
    * Added counting and merge sort.
    
    * Added data to ReadMe file
    
    * added data to read me file.
    
    * Added Radix sort
    
    * Added changes to README file.
    
    * Update RadixSort.cs
    
    * Added HeapSort and BubbleSortV2
    
    * Added unit tests for bubble sort v1
    
    * Moved bubble sort v2 to bubble sort folder
    
    * Fixed unit test issues
    
    * Added merge sort v2
    
    * Added counting sort v2
    
    * Modified README
    
    * Added radix sort v2
    
    * Fixed build issue
    
    * Removed RadixSort file
    
    * Added quick sort and quick select algorithm
    
    * Added KMP algorithm V2
    
    Co-authored-by: Pavan Kumar Koppineni <pavankoppineni@gmail.com>

---
+++ src/Algorithms.All/ManachersAlgorithm/ManachersAlgorithmV1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.All.ManachersAlgorithm
+{
+    /// <summary>
+    /// https://medium.com/hackernoon/manachers-algorithm-explained-longest-palindromic-substring-22cb27a5e96f
+    /// </summary>
+    public class ManachersAlgorithmV1
+    {
+        public string FindLongestPalindrome(string str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Algorithms.PatternMatching.Tests/KmpAlgorithm/KMPAlgorithmV1Tests.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms.PatternMatching.KmpAlgorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.PatternMatching.Tests.KmpAlgorithm
+{
+    [TestClass]
+    public class KMPAlgorithmV1Tests
+    {
+        [DataTestMethod]
+        [DataRow("abcxyzd", "xyz", true)]
+        [DataRow("abcxyd", "xyz", false)]
+        [DataRow("abcxd", "cx", true)]
+        public void Given_TextAndPattern_When_Find_Then_ShouldReturnTrueIfPatternExistsElseFalse(string text, string pattern, bool expectedResult)
+        {
+            //Given
+            var kmpAlgorithm = new KMPAlgorithmV2();
+
+            //When
+            var actualResult = kmpAlgorithm.PatternExists(text, pattern);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
