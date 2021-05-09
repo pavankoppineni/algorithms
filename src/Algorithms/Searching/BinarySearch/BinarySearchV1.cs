b35af2104aa82850c0669fe8af4e457aa901305b
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-05-09 20:20:12 +0530

    Added Binary Search Algorithm

---
+++ src/Algorithms/Searching/BinarySearch/BinarySearchV1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Searching.BinarySearch
+{
+    public class BinarySearchV1
+    {
+        public int Find(int[] values, int value)
+        {
+            return Find(values, 0, values.Length - 1, value);
+        }
+
+        private int Find(int[] values, int start, int end, int value)
+        {
+            if (start > end)
+            {
+                return -1;
+            }
+
+            var mid = (end - start + 1) >> 1;
+
+            //Case 1 : When value is equal to mid element
+            //Action : Return mid index
+            if (values[mid] == value)
+            {
+                return mid;
+            }
+
+            //Case 2 : When value is greater than mid element
+            //Action : Search right half
+            if (value > values[mid])
+            {
+                return Find(values, mid + 1, end, value);
+            }
+
+            //Case 3 : When value is less than mid element
+            //Action : Search left half
+            return Find(values, start, mid - 1, value);
+        }
+    }
+}
---
+++ test/Algorithms.Tests/Searching/BinarySearch/BinarySearchV1Tests.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms.Searching.BinarySearch;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.Searching.BinarySearch
+{
+    [TestClass]
+    public class BinarySearchV1Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArrayAndValue_When_SearchIntegerArray_Then_ReturnIndexOfValue()
+        {
+            //Given
+            var values = new int[] { 2, 5, 8, 10, 12, 15 };
+            var value = -2;
+            var expectedIndex = -1;
+            var binarySearch = new BinarySearchV1();
+
+            //When
+            var actualIndex = binarySearch.Find(values, value);
+
+            //Then
+            Assert.AreEqual(expectedIndex, actualIndex);
+        }
+    }
+}
