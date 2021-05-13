910351383f4cd2390dbe20e18ec5e1cb5b2a8394
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-05-13 13:38:25 +0530

    Added preorder traversal algorithm

---
+++ src/Algorithms/Traversals/Tree/Inorder/InorderTraversalUsingStackV2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Traversals.Tree.Inorder
+{
+    public class InorderTraversalUsingStackV2
+    {
+        public IList<int> Traverse(Node node)
+        {
+            var result = new List<int>();
+            var stack = new Stack<Node>();
+            var currentNode = node;
+            while (currentNode != null || stack.Count > 0)
+            {
+                while (currentNode != null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.Left;
+                }
+
+                currentNode = stack.Pop();
+                result.Add(currentNode.Value);
+                currentNode = currentNode.Right;
+            }
+            return result;
+        }
+    }
+}
---
+++ src/Algorithms/Traversals/Tree/Preorder/PreorderTraversalUsingRecursionV1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Traversals.Tree.Preorder
+{
+    public class PreorderTraversalUsingRecursionV1
+    {
+        public IList<int> Traverse(Node node)
+        {
+            return Traverse(node, new List<int>());
+        }
+
+        private IList<int> Traverse(Node node, IList<int> values)
+        {
+            if (node == null)
+            {
+                return values;
+            }
+            values.Add(node.Value);
+            Traverse(node.Right, values);
+            Traverse(node.Left, values);
+            return values;
+        }
+    }
+}
