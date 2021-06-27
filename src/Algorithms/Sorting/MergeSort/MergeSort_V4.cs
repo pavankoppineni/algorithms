7fe71b47b85f628b6311f27f8b36cd001c2870a9
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-06-27 13:09:39 +0530

    Added merge sort v4

---
+++ src/Algorithms/Sorting/MergeSort/MergeSort_V4.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting.MergeSort
+{
+    public class MergeSort_V4
+    {
+        public int[] Sort(int[] values)
+        {
+            Divide(0, values.Length - 1, values);
+            return values;
+        }
+
+        private void Divide(int start, int end, int[] values)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            var mid = start + ((end - start) >> 1);
+            Divide(start, mid, values);
+            Divide(mid + 1, end, values);
+            Merge(start, mid, mid + 1, end, values);
+        }
+
+        private void Merge(int startOne, int endOne, int startTwo, int endTwo, int[] values)
+        {
+            var arrayOnePointer = 0;
+            var arrayTwoPointer = 0;
+            var arrayOneLength = endOne - startOne + 1;
+            var arrayTwoLength = endTwo - startTwo + 1;
+            var arrayOne = new int[arrayOneLength];
+            var arrayTwo = new int[arrayTwoLength];
+            Array.Copy(values, startOne, arrayOne, arrayOnePointer, arrayOneLength);
+            Array.Copy(values, startTwo, arrayTwo, arrayTwoPointer, arrayTwoLength);
+
+            while (arrayOnePointer < arrayOneLength && arrayTwoPointer < arrayTwoLength)
+            {
+                //Case 1 : When arrayOnePointer is less than arrayTwoPointer
+                //Action : Increase arrayOnePointer
+                if (arrayOne[arrayOnePointer] < arrayTwo[arrayTwoPointer])
+                {
+                    values[startOne] = arrayOne[arrayOnePointer];
+                    startOne += 1;
+                    arrayOnePointer += 1;
+                    continue;
+                }
+
+                //Case 2 : When arrayOnePointer is greater than arrayTwoPointer
+                //Action : Increase arrayTwoPointer
+                if (arrayOne[arrayOnePointer] > arrayTwo[arrayTwoPointer])
+                {
+                    values[startOne] = arrayTwo[arrayTwoPointer];
+                    startOne += 1;
+                    arrayTwoPointer += 1;
+                    continue;
+                }
+
+                if (arrayOne[arrayOnePointer] == arrayTwo[arrayTwoPointer])
+                {
+                    values[startOne] = arrayOne[arrayOnePointer];
+                    startOne += 1;
+                    values[startOne] = arrayTwo[arrayTwoPointer];
+                    startOne += 1;
+                    arrayOnePointer += 1;
+                    arrayTwoPointer += 1;
+                }
+            }
+
+            while (arrayOnePointer < arrayOneLength)
+            {
+                values[startOne] = arrayOne[arrayOnePointer];
+                startOne += 1;
+                arrayOnePointer += 1;
+            }
+
+            while (arrayTwoPointer < arrayTwoLength)
+            {
+                values[startOne] = arrayTwo[arrayTwoPointer];
+                startOne += 1;
+                arrayTwoPointer += 1;
+            }
+        }
+    }
+}
---
+++ test/Algorithms.Tests/Sorting/MergeSort/MergeSort_V4Tests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Sorting.MergeSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.Sorting.MergeSort
+{
+    [TestClass]
+    public class MergeSort_V4Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_Sort_Then_ShouldReturnSortedArray()
+        {
+            //Given
+            var values = new int[] { 3, 2, 1, 3, 2 };
+            var problem = new MergeSort_V4();
+            var expectedValues = new int[] { 1, 2, 2, 3, 3 };
+
+            //When
+            var actualValues = problem.Sort(values);
+
+            //Then
+            for (int i = 0; i < values.Length; i++)
+            {
+                Assert.AreEqual(expectedValues[i], actualValues[i]);
+            }
+        }
+    }
+}
