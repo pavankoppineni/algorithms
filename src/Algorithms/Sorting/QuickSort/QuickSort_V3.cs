d0bf6f769380160bc845ad86ee8c5d3060428bcb
Pavan Kumar Koppineni <pkoppineni@azure.uworld.com>
2021-06-27 22:52:37 +0530

    Added quick sort v3

---
+++ src/Algorithms/Sorting/QuickSort/QuickSort_V3.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting.QuickSort
+{
+    public class QuickSort_V3
+    {
+        public int[] Sort(int[] values)
+        {
+            Sort(0, values.Length - 1, values);
+            return values;
+        }
+
+        private void Sort(int start, int end, int[] values)
+        {
+            if (start >= end)
+            {
+                return;
+            }
+            var partition = FindPartition(start, end - 1, end, values);
+            Sort(start, partition - 1, values);
+            Sort(partition + 1, end, values);
+        }
+
+        private int FindPartition(int start, int end, int pivot, int[] values)
+        {
+            var currentPartition = start;
+            while (start <= end)
+            {
+                //Case 1 : When start is less than pivot
+                if (values[start] <= values[pivot])
+                {
+                    var temp = values[currentPartition];
+                    values[currentPartition] = values[start];
+                    values[start] = temp;
+                    start += 1;
+                    currentPartition += 1;
+                }
+                else
+                {
+                    start += 1;
+                }
+            }
+
+            var t = values[currentPartition];
+            values[currentPartition] = values[pivot];
+            values[pivot] = t;
+            return currentPartition;
+        }
+    }
+}
---
+++ test/Algorithms.Tests/Sorting/QuickSort/QuickSort_V3Tests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Sorting.QuickSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.Sorting.QuickSort
+{
+    [TestClass]
+    public class QuickSort_V3Tests
+    {
+        [TestMethod]
+        public void Given_ArrayOfIntegers_When_Sort_Then_ShouldReturnSortedArray()
+        {
+            //Given
+            var values = new int[] { 3, 2, 1, 5, 2 };
+            var quickSort = new QuickSort_V3();
+            var expectedValues = new int[] { 1, 2, 2, 3, 5 };
+
+            //When
+            var actualValues = quickSort.Sort(values);
+
+            //Then
+            for (var index = 0; index < actualValues.Length; index++)
+            {
+                Assert.AreEqual(expectedValues[index], actualValues[index]);
+            }
+        }
+    }
+}
