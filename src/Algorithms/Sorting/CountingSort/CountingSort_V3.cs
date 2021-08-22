e6ba0e8cf6a72b9ce966d990ae13a8615863d13b
pkoppineni <pkoppineni@csod.com>
2021-08-22 19:28:06 +0530

    Added counting sort

---
+++ src/Algorithms/Sorting/CountingSort/CountingSort_V3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Sorting.CountingSort
+{
+    public class CountingSort_V3
+    {
+        public int[] Sort(int[] values)
+        {
+            if (values.Length == 1)
+            {
+                return values;
+            }
+
+            var max = values[0];
+            foreach (var value in values)
+            {
+                if (value > max)
+                {
+                    max = value;
+                }
+            }
+
+            var countingArray = new int[max];
+            for (var index = 0; index < values.Length; index++)
+            {
+                var value = values[index];
+                countingArray[value - 1] += 1;
+            }
+
+            for (var index = 1; index < countingArray.Length; index++)
+            {
+                countingArray[index] += countingArray[index - 1];
+            }
+
+            var newArray = new int[values.Length];
+            for (var index = 0; index < values.Length; index++)
+            {
+                var value = values[index];
+                var position = countingArray[value - 1];
+                countingArray[value - 1] -= 1;
+                newArray[position - 1] = value;
+            }
+
+            return newArray;
+        }
+    }
+}
---
+++ test/Algorithms.Tests/Sorting/CountingSort/CountingSort_V3Tests.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Sorting.CountingSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.Sorting.CountingSort
+{
+    [TestClass]
+    public class CountingSort_V3Tests
+    {
+        [TestMethod]
+        public void Given_IntegerArray_When_Sort_Then_ShouldReturnSortedArray()
+        {
+            //Given
+            var values = new int[] { 4, 1, 4, 7 };
+            var sort = new CountingSort_V3();
+            var expectedValues = new int[] { 1, 4, 4, 7};
+
+            //When
+            var actualValues = sort.Sort(values);
+
+            //Then
+            for(var index = 0; index < values.Length; index++)
+            {
+                Assert.AreEqual(expectedValues[index], actualValues[index]);
+            }
+        }
+    }
+}
