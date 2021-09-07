1f5fc572e8ab3c822fe0dfb5c028dc5b58fa82f8
pkoppineni <pkoppineni@csod.com>
2021-09-07 17:52:33 +0530

    Added KMP algorithm

---
+++ src/Algorithms/PatternSearching/KMPAlgorithm/KMPAlgorithm_V1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.PatternSearching.KMPAlgorithm
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+    /// </summary>
+    public class KMPAlgorithm_V1
+    {
+        public int PatternExists(string text, string pattern)
+        {
+            var foundIndex = -1;
+            var lps = CalculateLPS(pattern);
+            var textIndex = 0;
+            var patternIndex = 0;
+            while (textIndex < text.Length)
+            {
+                //Case 1 : When textIndex and patternIndex are equal
+                if (text[textIndex] == pattern[patternIndex])
+                {
+                    textIndex += 1;
+                    patternIndex += 1;
+                    if (patternIndex == pattern.Length)
+                    {                        
+                        foundIndex = textIndex - pattern.Length;
+                        break;
+                    }
+                    continue;
+                }
+                if(patternIndex == 0)
+                {
+                    textIndex += 1;
+                }
+                else
+                {
+                    patternIndex = lps[patternIndex - 1];
+                }
+            }
+            return foundIndex;
+        }
+
+        private int[] CalculateLPS(string pattern)
+        {
+            var lps = new int[pattern.Length];
+            lps[0] = 0;
+            var suffixIndex = 1;
+            var prefixIndex = 0;
+            while (suffixIndex < pattern.Length)
+            {
+                //Case 1 : When suffix index and prefix index are equal
+                if (pattern[suffixIndex] == pattern[prefixIndex])
+                {
+                    prefixIndex++;
+                    lps[suffixIndex] = prefixIndex;
+                    suffixIndex++;
+                    continue;
+                }
+
+                if (prefixIndex == 0)
+                {
+                    suffixIndex += 1;
+                }
+                else
+                {
+                    prefixIndex = lps[prefixIndex - 1];
+                }
+            }
+
+            return lps;
+        }
+    }
+}
---
+++ test/Algorithms.Tests/PatternSearching/KMPAlgorithm/KMPAlgorithm_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms.PatternSearching.KMPAlgorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.PatternSearching.KMPAlgorithm
+{
+    [TestClass]
+    public class KMPAlgorithm_V1Tests
+    {
+        [TestMethod]
+        public void Given_PatternAndText_When_SearchForPattern_Then_ShouldReturnTrueIfPatternExists()
+        {
+            //Given
+            var text = "ababxababc";
+            var pattern = "ababc";
+            var problem = new KMPAlgorithm_V1();
+            var expectedResult = 5;
+
+            //When
+            var actualResult = problem.PatternExists(text, pattern);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
