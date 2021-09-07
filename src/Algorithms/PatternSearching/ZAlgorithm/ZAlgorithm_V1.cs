7e3c93e4eafbf13c6452facba869f22359cda612
pkoppineni <pkoppineni@csod.com>
2021-09-07 18:41:56 +0530

    Added ZArray algorithm

---
+++ src/Algorithms/PatternSearching/ZAlgorithm/ZAlgorithm_V1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.PatternSearching.ZAlgorithm
+{
+    /// <summary>
+    /// https://www.geeksforgeeks.org/z-algorithm-linear-time-pattern-searching-algorithm/
+    /// </summary>
+    public class ZAlgorithm_V1
+    {
+        public int SearchPattern(string text, string pattern)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ test/Algorithms.Tests/PatternSearching/ZAlgorithm/ZAlgorithm_V1Tests.cs
@@ -0,0 +1,28 @@
+﻿using Algorithms.PatternSearching.ZAlgorithm;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Tests.PatternSearching.ZAlgorithm
+{
+    [TestClass]
+    public class ZAlgorithm_V1Tests
+    {
+        [TestMethod]
+        public void Given_TextAndPattern_When_SearchPattern_Then_ShouldReturnIndexOfPattern()
+        {
+            //Given
+            var text = "ababcababx";
+            var pattern = "ababx";
+            var problem = new ZAlgorithm_V1();
+            var expectedResult = 5;    
+
+            //When
+            var actualResult = problem.SearchPattern(text, pattern);
+
+            //Then
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
